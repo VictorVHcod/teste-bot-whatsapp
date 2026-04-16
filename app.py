ea0eef0895464059e900da5296d236106b61717b
VictorVHcod <victorhenrique8877@gmail.com>
2026-04-16 05:40:23 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    mensagem = data.get("text", {}).get("message", "") or ""
+    mensagem = mensagem.lower()
+    telefone = data.get("phone")
+
+    if "oi" in mensagem:
+        resposta = """
+        Oi! Tudo bem? Como posso te ajudar ?
+
+        [1] - Agendamento
+        [2] - dúvida
+
+        """
+    elif mensagem == "1":
+        resposta = "Qual data deseja agendar?"
+    elif mensagem == "2":
+        resposta = "É apenas um estudo de programador, TMJ"
+    else:
+        return {"status": "ok"}
+
+    return jsonify({
+        "phone": telefone,
+        "message": resposta
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
